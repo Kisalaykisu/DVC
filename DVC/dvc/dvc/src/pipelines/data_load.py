fa7c04bb71e7589308d30a4043e6c3fe5672a73d
Kisalay <kisalaykisu@gmail.com>
2021-09-24 02:36:24 -0700

    Update data_load.py

--- DVC/dvc/dvc/src/pipelines/data_load.py
+++ DVC/dvc/dvc/src/pipelines/data_load.py
@@ -1,9 +1,15 @@
 import argparse
 from typing import Text
 import yaml
-
+import logging
+import os
 from src.data.dataset import get_dataset
 
+
+logging_str="[% (asctime)s: %(levelname)s: %(module)s] %(module)s"
+log_dir="logs"
+os.makedirs(log_dir,exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir,"running_logs.log",level=logging.INFO,format=logging_str))
 
 def data_load(config_path: Text) -> None:
     """ Load raw data.
@@ -11,7 +17,7 @@ def data_load(config_path: Text) -> None:
         config_path{Text}:path to config
         base_config_path{Text}:path to base config
     """
-
+    logging.info("Let start the creation of data load pipeline:")
     config = yaml.safe_load(open(config_path))
     dataset = get_dataset()
     dataset.to_csv(config['data_load']['dataset_csv'], index=False)
